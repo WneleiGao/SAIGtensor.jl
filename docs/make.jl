88d62e0476ddaeeae142d59d59f3324b0d17a952
WneLeiGao <wgao1@ualberta.ca>
2017-02-26 21:19:16 -0700

    change some thing

--- docs/make.jl
+++ docs/make.jl
@@ -2,12 +2,16 @@ using Documenter, SAIGtensor
 
 makedocs(
     modules = [SAIGtensor],
-    clean = false,
+    format  = :html,
+    sitename = "SAIGtensor.jl",
+    doctest  = true,
+    strict   = true
 )
 
 deploydocs(
     deps   = Deps.pip("mkdocs", "python-markdown-math"),
     repo   = "github.com/WneleiGao/SAIGtensor.jl.git",
-    julia  = "0.5",
-    osname = "linux"
+    julia  = "release",
+    make   = nothing,
+    target = "build"
 )
